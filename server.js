62d7bd78ad1d71afd56613113c1552f52a3a671b
Austyn Whaley <70417091+austynwhaley@users.noreply.github.com>
2021-01-10 21:45:44 -0500

    server.js updated with post delete get functions needs to be finished

--- server.js
+++ server.js
@@ -4,9 +4,23 @@ var path = require("path");
 var app = express();
 var PORT = 3000;
 
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "./Develop/public")));
+
+
+let notes = [{
+    id: "",
+    title: "First note",
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, illo ratione magnam perferendis consequuntur enim! Eaque, rerum libero, tempore distinctio natus omnis, necessitatibus facilis earum molestias dolorem voluptates similique sequi."
+},
+{
+    id: "",
+    title: "Second note",
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, illo ratione magnam perferendis consequuntur enim! Eaque, rerum libero, tempore distinctio natus omnis, necessitatibus facilis earum molestias dolorem voluptates similique sequi."
+}
+]
 
 
 //routes
@@ -16,12 +30,28 @@ app.get("/notes", function (req, res) {
     
 });
 
-app.get("/", function (req, res) {
+app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
 });
+
+app.get("/api/notes", function(req,res){
+    return res.json(notes)
+
+})
+
+app.post("/api/notes",function(){
+    
+})
+
+app.delete("/api/notes/:id", function(req,res){
+  var deleted = req.params.id
+  console.log(deleted);
+
+})
 
 
 //listener
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
-  });
+});
+
