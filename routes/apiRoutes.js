ebde2d88c4bcc326c2f39706d0758465d9363973
Austyn Whaley <70417091+austynwhaley@users.noreply.github.com>
2021-01-11 23:26:57 -0500

    post function worked out

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,13 +2,28 @@ const fs = require("fs");
 
 module.exports = function(app) {
 
+  let notes = require("../db/db.json")
+
   app.get("/api/notes", function(req,res){
-    return res.json(notes)
+    return res.json(notes)})
 
-  })
+  app.post("/api/notes",function(req,res){
+    const newNote = req.body
 
-  app.post("/api/notes",function(){
-    
+    if (notes.length === 0){newNote.id = 1} 
+
+    notes.push(newNote);
+
+    let jsonNotes = JSON.stringify(notes)
+
+    fs.writeFile("./db/db.json", jsonNotes, function(err) {
+
+    console.log("Note saved");
+
+    })
+
+    res.json(true)
+
   })
 
   app.delete("/api/notes/:id", function(req,res){
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,8 +1,8 @@
-//Set up dependencies
 var path = require("path");
 
 module.exports = function(app) {
 
-  app.get("*", function(req, res) {res.sendFile(path.join(__dirname, "../public/index.html"));});
+  app.get("/", function(req, res) {res.sendFile(path.join(__dirname, "../public/index.html"));});
 
-  app.get("/notes", function(req, res) {res.sendFile(path.join(__dirname, "../public/notes.html"));});};
+  app.get("/notes", function(req, res) {res.sendFile(path.join(__dirname, "../public/notes.html"));});
+};
