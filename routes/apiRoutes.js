3f5c54c17dd46785639d7d3fff71e3937e844f10
Austyn Whaley <70417091+austynwhaley@users.noreply.github.com>
2021-01-12 21:29:18 -0500

    post id method changed and typos corrected

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const uuid = require('uuid');
 let notesArray = require("../db/db.json")
 
 module.exports = function(app) {
@@ -11,34 +12,28 @@ module.exports = function(app) {
   app.post("/api/notes",function(req,res){
     const newNote = req.body
 
-    if (notesArray.length === 0){newNote.id = 1} 
-    else {newNote.id = (notesArray[notesArray.length-1].id + 1)}
+    newNote.id = uuid.v4();
     
     notesArray.push(newNote);
 
     let jsonNotes = JSON.stringify(notesArray)
 
-    fs.writeFile("./db/db.json", jsonNotes, function(err) {
-      if(err) throw err
-      console.log("Note saved");
-    })
+    fs.writeFile("./db/db.json", jsonNotes, function(err) {if(err) throw err; console.log("Note saved");})
 
     res.json(true)
-
   })
 
-//deletes note from array depending on object 
-  app.delete("/api/notes/:id", (req, res) => {
+//deletes note from array depending on object id
+  app.delete("/api/notes/:id", function (req, res) {
     const id = req.params.id;
 
-    notesArray.forEach((selectedNote, idNum) => {
+    notesArray.forEach(function (selectedNote, idNum) {
       if(id == selectedNote.id){notesArray.splice(idNum,1)
 
         const dbFile = notesArray.slice();
+        let jsonNotes = JSON.stringify(dbFile)
 
         console.log(dbFile)
-
-        let jsonNotes = JSON.stringify(dbFile)
 
         fs.writeFile("./db/db.json", jsonNotes, function(err) {if (err) throw err; console.log("Note deleted");})
       }
